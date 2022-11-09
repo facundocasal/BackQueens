d7006cf2824329430360f9600cf6c26c60983d5f
Facundo Nahuel Casal <fcasal92@gmail.com>
2022-11-09 00:32:28 -0300

    small fixed

--- controllers/queen.js
+++ controllers/queen.js
@@ -6,7 +6,6 @@ const bcrypt = require('bcrypt');
 
 const createQueen = async (req, res) => {
   const errors = validationResult(req)
-  console.log(errors)
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: 'Algo salió mal' })
   }
@@ -27,8 +26,6 @@ const createQueen = async (req, res) => {
     })
     const salt = bcrypt.genSaltSync()
     newUserQueen.password = bcrypt.hashSync(password, salt)
-    console.log(newQueen)
-    console.log(newUserQueen)
     await newUserQueen.save()
     await newQueen.save()
     res.status(200).json(`Queen created successfully`)
